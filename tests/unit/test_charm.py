9be5ca827f4b44f09f47dd3f6de51734e28a937c
yazansalti <yazan.salti@canonical.com>
2023-11-26 23:57:58 +0400

    Adds unit tests

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -8,6 +8,7 @@ from typing import List
 from unittest.mock import Mock, call, patch
 
 import hcl  # type: ignore[import-untyped]
+from botocore.exceptions import ClientError  # type: ignore[import-untyped]
 from ops import testing
 from ops.model import ActiveStatus, WaitingStatus
 
